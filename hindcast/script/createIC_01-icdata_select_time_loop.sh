f4a3aeb2b7f7a1ca5f21cf60688b379b01b120e9
Yi-Hsuan Chen <yihsuan@umich.edu>
2024-01-28 09:44:30 +0800

    add datapath

--- hindcast/script/createIC_01-icdata_select_time_loop.sh
+++ hindcast/script/createIC_01-icdata_select_time_loop.sh
@@ -23,10 +23,13 @@
 
 #--- time in the file
 yyyy=2001
-mm=07
+mm=06
 hh="00Z"
 dd_start=1
-dd_end=31
+dd_end=30
+
+#--- data path
+datapath="/lfs/home/yihsuanc/data/data.TaiESM1_hindcast/data.July2001_ERA5.hindcast"  # on RCEC 300T
 
 #--- input files contain one-month data. You can download the data from ERA5 website and rename them.  
 filenames=("ERA5_PRS.t_q_u_v.${yyyy}${mm}_${hh}.r1440x721.nc" "ERA5_SFC.sp.${yyyy}${mm}_${hh}.r1440x721.nc")
@@ -45,14 +48,14 @@ num_files=$((${#filenames[@]}))
 
 for((j=0; j<$num_files; j=j+1))
 do
-  fname=${filenames[$j]}
+  fname=${datapath}/${filenames[$j]}
   new_head=${newfilehead[$j]}
 
   for ((i=${dd_start}; i<=${dd_end}; i=i+1))
   do
     dd=$(printf "%02d" $i)
     ss="${yyyy}_${mm}_${dd}_${hh}"
-    file1=${new_head}${ss}${newfileend}
+    file1=${datapath}/${new_head}${ss}${newfileend}
 
     #echo $fname, $file1
     comd="ncks -F -d time,$i $fname $file1"
